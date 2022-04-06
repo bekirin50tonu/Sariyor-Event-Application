7b6f1c0f5694f1b3ad3fda7c756ce9e1ed0209f2
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-07 00:10:20 +0300

    Create web_service.dart

---
+++ sariyor/lib/utils/web_service/web_service.dart
@@ -0,0 +1,33 @@
+import 'dart:html';
+import 'package:dio/dio.dart';
+import 'package:sariyor/utils/locale/shared_preferences.dart';
+
+class WebService {
+  static Dio? _inst;
+
+  static Dio init() {
+    return _inst ?? Dio();
+  }
+
+  static Dio? getInstance() {
+    _inst!.clear();
+    _inst!.interceptors.add(InterceptorsWrapper(
+        onRequest: (options, handler) => handler.next(options),
+        onError: (error, handler) async {
+          if (error.response?.statusCode == HttpStatus.unauthorized) {
+            var token = Prefs.getString('token');
+            error.requestOptions.headers['Authorization'] = "Bearer $token";
+            final opt = Options(
+                method: error.requestOptions.method,
+                headers: error.requestOptions.headers);
+            final req = await _inst!.request(error.requestOptions.path,
+                options: opt,
+                data: error.requestOptions.data,
+                queryParameters: error.requestOptions.queryParameters);
+            return handler.resolve(req);
+          }
+          handler.next(error);
+        }));
+    return _inst;
+  }
+}

dfdcab358446eb169184636c9a82368e1b5cc2d5
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-07 00:10:11 +0300

    Create theme_manager.dart

---
+++ sariyor/lib/themes/theme_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/diagnostics.dart';
+import 'package:flutter/src/cupertino/theme.dart';
+
+class ThemeManager {
+  static ThemeData lightTheme() {
+    return ThemeData(primaryColor: Colors.deepPurple);
+  }
+
+  static ThemeData darkTheme() {
+    return ThemeData(primaryColor: Colors.black);
+  }
+}

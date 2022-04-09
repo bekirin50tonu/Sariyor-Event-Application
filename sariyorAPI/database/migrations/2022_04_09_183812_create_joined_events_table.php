dc9883aef6abfb55c8bf39d2227ee3ece5c9fc5c
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-10 01:21:30 +0300

    Create 2022_04_09_183812_create_joined_events_table.php

---
+++ sariyorAPI/database/migrations/2022_04_09_183812_create_joined_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('joined_events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('joined_events');
+    }
+};

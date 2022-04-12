89ea1d6fae493f8fc490ee8bf89d7d2aa7da338d
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-12 18:02:02 +0300

    fixed css files and dashboard site.

--- sariyorAPI/resources/views/Admin/Components/category_table.blade.php
+++ sariyorAPI/resources/views/Admin/Components/category_table.blade.php
@@ -1,4 +1,4 @@
-<table>
+<table class="table">
     <thead>
     <tr>
         <th scope="col">#</th>
@@ -13,8 +13,8 @@
             <th scope="row">{{$category->id}}</th>
             <td>
                 @if($category->image_path)
-                    <img src="{{route('imagecategory',$category->image_path)}}" width="60"
-                         height="60"
+                    <img src="{{route('imagecategory',$category->image_path)}}" width="100"
+                         height="100"
                          alt="{{$category->name}}">
                 @else
                     <p>Nope</p>,
@@ -22,13 +22,15 @@
             </td>
             <td>{{$category->name}}</td>
             <td>
-                <button class="btn btn-warning" data-bs-toggle="modal"
-                        data-bs-target="#exampleModal{{$category->id}}">Update
-                </button>
-                <form action="{{route('admincategorydelete',$category->id)}}" method="POST">
-                    @csrf
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
+                <div class="d-flex gap-4">
+                    <button class="btn btn-warning" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal{{$category->id}}">Update
+                    </button>
+                    <form action="{{route('admincategorydelete',$category->id)}}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </div>
                 @include('admin.components.modal_form')
             </td>
         </tr>

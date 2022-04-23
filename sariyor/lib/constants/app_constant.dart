f1748819ff3a2b0ef0535ada3e417ef2f97cbd99
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-23 15:55:52 +0300

    added urls.

--- sariyor/lib/constants/app_constant.dart
+++ sariyor/lib/constants/app_constant.dart
@@ -1,3 +1,6 @@
 class AppConstants {
-  static String BASE_URL = "http://192.168.195.10:8000/api";
+  static const BASE_URL = "http://192.168.195.10:8000/api";
+  static const EVENT_IMAGE_URL = "http://192.168.195.10:8000/image/event/";
+  static const PROFILE_IMAGE_URL = "http://192.168.195.10:8000/image/profile/";
+  static const CATEGORY_IMAGE_URL = "http://192.168.195.10:8000/image/category/";
 }
--- sariyor/lib/constants/url_constant.dart
+++ sariyor/lib/constants/url_constant.dart
@@ -1,4 +1,6 @@
 class URLConstants {
+
+  
   static const register = '/auth/register';
   static const login = '/auth/login';
   static const logout = '/auth/logout';

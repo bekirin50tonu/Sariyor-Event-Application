fc556fd5863182422929582709afdeaecc161db7
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-23 15:56:00 +0300

    Update index_page.dart

--- sariyor/lib/features/events/pages/index_page.dart
+++ sariyor/lib/features/events/pages/index_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sariyor/features/events/cubit/event_cubit.dart';
+import 'package:sariyor/utils/web_service/web_service.dart';
 
 class IndexPage extends StatelessWidget {
-  const IndexPage({Key? key}) : super(key: key);
-
+  IndexPage({Key? key}) : super(key: key);
+  final GlobalKey _formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('TEXT'),
+    return BlocProvider(
+      create: (context) => EventCubit(WebService.getInstance(), context),
+      child: Scaffold(
+          body: BlocBuilder<EventCubit, BaseState>(
+        builder: (context, state) => buildEventPage(context, state),
+      )),
     );
+  }
+
+  Widget buildEventPage(BuildContext context, BaseState state) {
+    return Container();
   }
 }

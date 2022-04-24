1628d0f0731d41f5e4bfefd59adbe2e56a379fea
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-24 16:29:34 +0300

    Update theme_manager.dart

--- sariyor/lib/themes/theme_manager.dart
+++ sariyor/lib/themes/theme_manager.dart
@@ -4,7 +4,9 @@ import 'package:flutter/src/cupertino/theme.dart';
 
 class ThemeManager {
   static ThemeData lightTheme() {
-    return ThemeData(primaryColor: Colors.deepPurple);
+    return ThemeData(
+        primaryColor: Colors.deepPurple,
+        );
   }
 
   static ThemeData darkTheme() {

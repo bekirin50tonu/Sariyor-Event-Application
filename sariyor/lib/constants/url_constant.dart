64936109fe99fec3686e340b20edfec465a28d0f
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-27 17:01:21 +0300

    added update and get url.

--- sariyor/lib/constants/url_constant.dart
+++ sariyor/lib/constants/url_constant.dart
@@ -3,5 +3,8 @@ class URLConstants {
   static const login = '/auth/login';
   static const logout = '/auth/logout';
 
+  static const updateUser = '/user/update';
+  static const getUserData = '/user/get';
+
   static const get_all_joined_events = '/event/get_joined';
 }

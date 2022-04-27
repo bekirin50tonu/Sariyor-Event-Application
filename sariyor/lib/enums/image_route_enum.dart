f8ca477d6883117f67f64e93bae16d6a091c57c7
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2022-04-27 17:02:08 +0300

    fixed image error.

--- sariyor/lib/enums/image_route_enum.dart
+++ sariyor/lib/enums/image_route_enum.dart
@@ -3,14 +3,14 @@ import 'package:sariyor/constants/app_constant.dart';
 enum ImageRouteType { profile, event, category }
 
 extension ImageRouteExtension on ImageRouteType {
-  String url(String path) {
+  String url(String? path) {
     switch (this) {
       case ImageRouteType.profile:
-        return "${AppConstants.PROFILE_IMAGE_URL}$path";
+        return "${AppConstants.PROFILE_IMAGE_URL}${path ?? 'dummy'}";
       case ImageRouteType.event:
-        return "${AppConstants.EVENT_IMAGE_URL}$path";
+        return "${AppConstants.EVENT_IMAGE_URL}${path ?? 'dummy'}";
       case ImageRouteType.category:
-        return "${AppConstants.CATEGORY_IMAGE_URL}$path";
+        return "${AppConstants.CATEGORY_IMAGE_URL}${path ?? 'dummy'}";
       default:
         return "";
     }
